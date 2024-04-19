7b1f83c3f0f3fa92e297897212f95406846dbd8a
Rudresh-ZeroDistance <165876497+Rudresh-ZeroDistance@users.noreply.github.com>
2024-04-19 12:54:21 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+import { Server } from "socket.io"
+
+const io = new Server(3000, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+})
+
+const characters = []
+
+io.on("connection", (socket) => {
+
+    console.log("connected with socket id", socket.id)
+
+    characters.push({
+        id: socket.id,
+    })
+
+    socket.emit("hello",{
+        characters: characters,
+        id: socket.id,
+    })
+    
+    setInterval(() => {
+        socket.emit("ping", {
+            time : Date.now(),
+            characters: characters
+        })
+    },100)
+
+    socket.on("updateData", (data) => {
+        const index = characters.findIndex(c => c.id === socket.id);
+        if (index !== -1) {
+            characters[index].positionX = data.positionX;
+            characters[index].positionZ = data.positionZ;
+            characters[index].rotation = data.rotation;
+        }
+
+        
+
+    });
+
+    socket.on("disconnect", () => {
+        console.log("disconnected with socket id", socket.id)
+
+        characters.splice(characters.indexOf(characters.find(i => i.id == socket.id)), 1)
+        
+        io.emit("characters", characters)
+    })
+    
+
+})
